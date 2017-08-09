4cc2fd0c27f692fb9db9bd70c36ab8292adca5fb
bclarke98 <bclarke98@outlook.com>
2017-08-09 18:30:41 -0400

    Started organization

---
+++ bots.py
@@ -0,0 +1,47 @@
+import praw
+import random
+
+class RedditBot(object):
+    def __init__(self, details):
+        self.reddit = praw.Reddit(client_id=details['rcid'], client_secret=details['rcs'], user_agent='dex-bot by /u/overlysalty', username='d3x-bot', password=details['rpw'])
+
+        self.subreddits = {}    
+    def get_sub_image_data(self, subreddit):
+        try:
+            self.subreddits[subreddit][0]
+        except Exception as ex:  # if the subreddit hasn't been cached or is empty
+            submissions = self.reddit.subreddit(subreddit).hot(limit=50)
+            self.subreddits[subreddit] = [[subreddit, s.url, s.id, s.title] for s in submissions if ('.jpg' in s.url or '.png' in s.url)]
+        r = random.randint(0, len(self.subreddits[subreddit]) - 1)
+        return self.subreddits[subreddit].pop(r)
+        
+    def get_thread_url(self, data):
+        return 'www.reddit.com/r/' + data[0] + '/comments/' + data[2]
+    
+    def get_title(self, data):
+        return data[3]
+    
+    def get_image_url(self, data):
+        return data[1]
+
+
+class DiscordBot(object):
+    def __init__(self, details):
+        self.token = details['btkn']
+        self.client = details['client']
+        self.commands = {}
+    
+    def start(self):
+        self.client.loop.create_task(self.on_heartbeat())
+        self.client.run(self.token)
+
+    async def on_ready(self):
+        print('READY!')
+
+    async def on_message(self, message):
+        print(message)
+    
+    async def on_heartbeat(self):
+        pass
+
+
---
+++ startup.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+import sys
+import check
+if check.check_bot_on():
+    print('Bot already running!')
+    sys.exit()
+
+import os
+import discord
+import bots
+
+VERSION = '1.0.0'
+
+class DexManager:
+    def __init__(self, client):
+        self.details = {'client':client}
+        try:
+            with open('logins.dat', 'r') as lr:
+                self.details['btkn'], self.details['rcid'], self.details['rcs'], self.details['rpw'] = lr.read().strip().split('\n')
+        except IOError as ex:
+            print('"logins.dat" file not found. Please create one formatted as:')
+            print('Discord Bot Token')
+            print('Reddit Client ID')
+            print('Reddit Client Secret')
+            print('Reddit Password')
+        self.details['pid'] = str(os.getpid())
+        with open('pid.txt', 'w') as pidw:
+            pidw.write(self.details['pid'])
+        self.bot = bots.DiscordBot(self.details)
+        
+
+CLIENT = discord.Client()
+manager = DexManager(CLIENT)
+
+@CLIENT.event
+async def on_ready():
+    await manager.bot.on_ready()
+
+@CLIENT.event
+async def on_message(message):
+    await manager.bot.on_message(message)
+
+if __name__ == '__main__':
+    manager.bot.start()
+
+
